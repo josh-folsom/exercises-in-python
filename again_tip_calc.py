64b3cd95a42ff5efb295cb0d018d8a416407a333
josh-folsom <folsom.josh@gmail.com>
2017-04-23 11:45:15 -0500

    commit 2

---
+++ again_tip_calc.py
@@ -0,0 +1,17 @@
+bill = int(input("What was the bill amount?: $"))
+service = input("What was the level of service? good / fair / bad : ")
+
+if service != "good" and service != "fair" and service != "bad":
+    print("Incorrect input, try again")
+
+if service == "good":
+    totalbill = float(bill * 1.2)
+    print("Total bill is ${}".format(totalbill))
+
+if service == "fair":
+    totalbill = float(bill * 1.15)
+    print("Total bill is ${}".format(totalbill))
+
+if service == "bad":
+    totalbill = float(bill * 1.1)
+    print("Total bill is ${}".format(totalbill))
