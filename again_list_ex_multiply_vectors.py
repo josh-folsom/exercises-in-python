eb91443d6d5dfc4d2d65f77632d8f6170ef1f117
josh-folsom <folsom.josh@gmail.com>
2017-04-23 18:17:38 -0500

    commit 6

---
+++ again_list_ex_multiply_vectors.py
@@ -0,0 +1,4 @@
+one = [2, 3, 4]
+two = [1, 2, 3]
+three = zip(one, two)
+print(three)
---
+++ again_string_ex_multi.py
@@ -0,0 +1,6 @@
+asd = "this is a lowercase string".upper()
+print(asd)
+ask = "henry is not my name".capitalize()
+print(ask)
+back = "?detraf ohW"[::-1]
+print(back)
